d4d91722b3d1c7873cc951ffc3e5a7a0275cdbb3
Ola Tlałka <65178593+ATlalka@users.noreply.github.com>
2021-12-04 21:51:11 +0100

    update

--- 1_grudnia/pierwsze.java
+++ 1_grudnia/pierwsze.java
@@ -1,25 +1,21 @@
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class pierwsze {
 
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> pomiary = new ArrayList<Integer>();
-		
-		try (Scanner s = new Scanner(new File("pomiary.txt"))) {
 
-			//s.nextLine();
+		ArrayList<Integer> measurement = new ArrayList<Integer>();
+
+		try (Scanner s = new Scanner(new File("pomiary.txt"))) {
 
 			s.useDelimiter(";|\\r?\\n|\\r");
 
 			while (s.hasNext()) {
 
-				pomiary.add(s.nextInt());
+				measurement.add(s.nextInt());
 			}
 		}
 
@@ -27,16 +23,15 @@ public class pierwsze {
 			e.printStackTrace();
 			System.out.println("Blad e");
 		}
-		
-		int licznik = 0;
-		
-		for (int i = 0; i < pomiary.size() - 1; i++) {
-			if (pomiary.get(i) < pomiary.get(i+1))
-				licznik++;
+
+		int counter = 0;
+
+		for (int i = 0; i < measurement.size() - 1; i++) {
+			if (measurement.get(i) < measurement.get(i + 1))
+				counter++;
 		}
-		
-		System.out.println(licznik);
+
+		System.out.println(counter);
 
 	}
-
 }
